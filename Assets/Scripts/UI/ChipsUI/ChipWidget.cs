78b8d65aef5a3d00e6631889a518c299c3d5abd8
balkac <furkanbalkac@gmail.com>
2025-03-03 00:08:21 +0300

    ChipWidget updated

--- Assets/Scripts/UI/ChipsUI/ChipWidget.cs
+++ Assets/Scripts/UI/ChipsUI/ChipWidget.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -7,17 +9,30 @@ public class ChipWidget : MonoBehaviour
     [SerializeField] private int _chipValue;
     [SerializeField] private TextMeshProUGUI _chipText;
     [SerializeField] private Button _chipButton;
+    [SerializeField] private Image _highlightedImage;
+    [SerializeField] private Vector3 _selectedScale = new Vector3(1.2f, 1.2f, 1.2f);
+    [SerializeField] private float _animationDuration = 0.125f;
+
     private ChipManager _chipManager;
+    private Vector3 _originalScale;
+    private Coroutine _scaleCoroutine;
+
+    public Action<ChipWidget> OnChipSelected;
+    public Action OnChipDeselected;
+    private bool _isSelected = false;
 
     private void Awake()
     {
         _chipManager = ChipManager.Instance;
-        _chipButton.onClick.AddListener(OnChipSelected);
+        _chipButton.onClick.AddListener(OnChipButtonClicked);
+        _highlightedImage.enabled = false;
+        _originalScale = transform.localScale;
+        UpdateChipText();
     }
 
-    private void Start()
+    private void OnDestroy()
     {
-        UpdateChipText();
+        _chipButton.onClick.RemoveListener(OnChipButtonClicked);
     }
 
     private void UpdateChipText()
@@ -28,8 +43,57 @@ public class ChipWidget : MonoBehaviour
         }
     }
 
-    private void OnChipSelected()
+    private void OnChipButtonClicked()
     {
+        if (_isSelected)
+        {
+            DeselectChip();
+            OnChipDeselected?.Invoke();
+            return;
+        }
+
+        SelectChip();
+        OnChipSelected?.Invoke(this);
+    }
+
+    private void SelectChip()
+    {
+        _isSelected = true;
+        _highlightedImage.enabled = true;
         _chipManager.SetSelectedChip(_chipValue);
+        if (_scaleCoroutine != null)
+        {
+            StopCoroutine(_scaleCoroutine);
+        }
+
+        _scaleCoroutine = StartCoroutine(ScaleTo(_selectedScale));
+    }
+
+    public void DeselectChip()
+    {
+        _isSelected = false;
+        _highlightedImage.enabled = false;
+        _chipManager.SetSelectedChip(0);
+        if (_scaleCoroutine != null)
+        {
+            StopCoroutine(_scaleCoroutine);
+        }
+
+        _scaleCoroutine = StartCoroutine(ScaleTo(_originalScale));
+    }
+
+    private IEnumerator ScaleTo(Vector3 targetScale)
+    {
+        Vector3 startScale = transform.localScale;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < _animationDuration)
+        {
+            transform.localScale = Vector3.Lerp(startScale, targetScale, elapsedTime / _animationDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = targetScale;
     }
 }
--- Assets/Scripts/UI/ChipsUI/ChipsUI.cs
+++ Assets/Scripts/UI/ChipsUI/ChipsUI.cs
@@ -1,14 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ChipsUI : MonoBehaviour
 {
     [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private RouletteManager _rouletteManager;
+    private List<ChipWidget> _chipWidgets;
+    private ChipWidget _selectedChipWidget;
 
     private void Awake()
     {
+        _chipWidgets = new List<ChipWidget>(GetComponentsInChildren<ChipWidget>());
+
+        foreach (ChipWidget chipWidget in _chipWidgets)
+        {
+            chipWidget.OnChipSelected += OnChipSelected;
+            chipWidget.OnChipDeselected += OnChipDeselected;
+        }
+
         _rouletteManager.OnSpinStarted += OnSpinStarted;
         _rouletteManager.OnSpinCompleted += OnSpinCompleted;
+    }
+
+    private void OnChipDeselected()
+    {
+        _selectedChipWidget = null;
+    }
+
+    private void OnChipSelected(ChipWidget chipWidget)
+    {
+        if (_selectedChipWidget != null)
+        {
+            _selectedChipWidget.DeselectChip();
+        }
+
+        _selectedChipWidget = chipWidget;
     }
 
     private void OnSpinStarted()
@@ -23,6 +49,12 @@ public class ChipsUI : MonoBehaviour
 
     private void OnDestroy()
     {
+        foreach (ChipWidget chipWidget in _chipWidgets)
+        {
+            chipWidget.OnChipSelected -= OnChipSelected;
+            chipWidget.OnChipDeselected -= OnChipDeselected;
+        }
+
         _rouletteManager.OnSpinStarted -= OnSpinStarted;
         _rouletteManager.OnSpinCompleted -= OnSpinCompleted;
     }
