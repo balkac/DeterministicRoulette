77c076ba10de410815e0ac607b71b2eb3d1aea05
balkac <furkanbalkac@gmail.com>
2025-03-04 01:12:55 +0300

    OutSideButton widgets added

--- Assets/Scripts/BettingSystem/BetCondition.cs
+++ Assets/Scripts/BettingSystem/BetCondition.cs
@@ -7,27 +7,29 @@ public class BetCondition : ScriptableObject, IBetCondition
 
     public bool Evaluate(int winningNumber, int[] numbers)
     {
-        switch (BetType)
-        {
-            case BetType.Red: return IsRed(winningNumber);
-            case BetType.Black: return !IsRed(winningNumber) && winningNumber > 0;
-            case BetType.Even: return winningNumber % 2 == 0;
-            case BetType.Odd: return winningNumber % 2 != 0;
-            case BetType.High: return winningNumber >= 19;
-            case BetType.Low: return winningNumber <= 18;
-            case BetType.FirstDozen: return winningNumber >= 1 && winningNumber <= 12;
-            case BetType.SecondDozen: return winningNumber >= 13 && winningNumber <= 24;
-            case BetType.ThirdDozen: return winningNumber >= 25 && winningNumber <= 36;
-            case BetType.FirstColumn: return (winningNumber - 1) % 3 == 0;
-            case BetType.SecondColumn: return (winningNumber - 2) % 3 == 0;
-            case BetType.ThirdColumn: return (winningNumber - 3) % 3 == 0;
-            default: return System.Array.Exists(numbers, n => n == winningNumber);
-        }
+        return System.Array.Exists(numbers, n => n == winningNumber);
+
+        // switch (BetType)
+        // {
+        //     case BetType.Red: return IsRed(winningNumber);
+        //     case BetType.Black: return !IsRed(winningNumber) && winningNumber > 0;
+        //     case BetType.Even: return winningNumber % 2 == 0;
+        //     case BetType.Odd: return winningNumber % 2 != 0;
+        //     case BetType.High: return winningNumber >= 19;
+        //     case BetType.Low: return winningNumber <= 18;
+        //     case BetType.FirstDozen: return winningNumber >= 1 && winningNumber <= 12;
+        //     case BetType.SecondDozen: return winningNumber >= 13 && winningNumber <= 24;
+        //     case BetType.ThirdDozen: return winningNumber >= 25 && winningNumber <= 36;
+        //     case BetType.FirstColumn: return (winningNumber - 1) % 3 == 0;
+        //     case BetType.SecondColumn: return (winningNumber - 2) % 3 == 0;
+        //     case BetType.ThirdColumn: return (winningNumber - 3) % 3 == 0;
+        //     default: return System.Array.Exists(numbers, n => n == winningNumber);
+        // }
     }
 
-    private bool IsRed(int number)
-    {
-        int[] redNumbers = { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
-        return System.Array.Exists(redNumbers, n => n == number);
-    }
+    // private bool IsRed(int number)
+    // {
+    //     int[] redNumbers = { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
+    //     return System.Array.Exists(redNumbers, n => n == number);
+    // }
 }
